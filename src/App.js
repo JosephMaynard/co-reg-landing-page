62a9cfb3ff4d75d4dc9e35fb88135e7fb4842e4e
Joseph Maynard <joseph.maynard@mobileembrace.com>
2017-04-20 13:18:06 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { version } from 'inferno';
+import Component from 'inferno-component';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
