498a7c01019db154d5a495f2f9cf6ef3be180ec4
Joseph Maynard <joseph.maynard@mobileembrace.com>
2017-04-20 14:55:46 +1000

    Create logo add eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { version } from 'inferno';
 import Component from 'inferno-component';
+import Logo from './logo';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Logo
+                    style={{fill: '#fff'}}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
